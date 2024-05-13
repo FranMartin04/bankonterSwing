0092ae296c120bec43e840fe12d1720e152e92b2
Francisco <franciscojaviermartincorredera@gmail.com>
2024-05-13 11:28:01 +0200

    add

---
+++ src/main/java/entities/Entidad.java
@@ -0,0 +1,10 @@
+package entities;
+
+public abstract class Entidad {
+
+	public Entidad () {
+		super();
+	}
+	
+	public abstract int getId();	
+}
---
+++ src/main/java/paneles/PanelPrincipal.java
@@ -0,0 +1,193 @@
+package paneles;
+
+import javax.swing.JPanel;
+import java.awt.GridBagLayout;
+import javax.swing.JLabel;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import javax.swing.JTextField;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import javax.swing.JFormattedTextField;
+import javax.swing.JFrame;
+import javax.swing.JSpinner;
+import javax.swing.JSlider;
+import javax.swing.JButton;
+
+public class PanelPrincipal extends JPanel {
+
+	private static final long serialVersionUID = 1L;
+	private JTextField jtfDescripcion;
+	private JTextField jtfTipoContrato;
+	private JSpinner jspinner;
+	private JSlider jslider;
+
+	/**
+	 * Create the panel.
+	 */
+	public static void main(String[] args) {
+        JFrame frame = new JFrame("Panel Principal");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(600, 300);
+        frame.getContentPane().add(new PanelPrincipal());
+        frame.setVisible(true);
+    }
+	public PanelPrincipal() {
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.columnWidths = new int[] { 0, 0, 0, 0, 0, 0, 0, 0 };
+		gridBagLayout.rowHeights = new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+		gridBagLayout.columnWeights = new double[] { 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE };
+		gridBagLayout.rowWeights = new double[] { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE };
+		setLayout(gridBagLayout);
+
+		JLabel lblNewLabel = new JLabel("Contrato");
+		GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
+		gbc_lblNewLabel.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel.gridx = 3;
+		gbc_lblNewLabel.gridy = 0;
+		add(lblNewLabel, gbc_lblNewLabel);
+
+		JLabel lblNewLabel_1 = new JLabel("Descripcion:");
+		GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
+		gbc_lblNewLabel_1.anchor = GridBagConstraints.EAST;
+		gbc_lblNewLabel_1.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_1.gridx = 0;
+		gbc_lblNewLabel_1.gridy = 1;
+		add(lblNewLabel_1, gbc_lblNewLabel_1);
+
+		jtfDescripcion = new JTextField();
+		GridBagConstraints gbc_jtfDescripcion = new GridBagConstraints();
+		gbc_jtfDescripcion.gridwidth = 6;
+		gbc_jtfDescripcion.insets = new Insets(0, 0, 5, 0);
+		gbc_jtfDescripcion.fill = GridBagConstraints.HORIZONTAL;
+		gbc_jtfDescripcion.gridx = 1;
+		gbc_jtfDescripcion.gridy = 1;
+		add(jtfDescripcion, gbc_jtfDescripcion);
+		jtfDescripcion.setColumns(10);
+
+		JLabel lblNewLabel_2 = new JLabel("Fecha:");
+		GridBagConstraints gbc_lblNewLabel_2 = new GridBagConstraints();
+		gbc_lblNewLabel_2.anchor = GridBagConstraints.EAST;
+		gbc_lblNewLabel_2.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_2.gridx = 0;
+		gbc_lblNewLabel_2.gridy = 2;
+		add(lblNewLabel_2, gbc_lblNewLabel_2);
+
+		JFormattedTextField jftfFecha = new JFormattedTextField();
+		GridBagConstraints gbc_jftfFecha = new GridBagConstraints();
+		gbc_jftfFecha.gridwidth = 6;
+		gbc_jftfFecha.insets = new Insets(0, 0, 5, 0);
+		gbc_jftfFecha.fill = GridBagConstraints.HORIZONTAL;
+		gbc_jftfFecha.gridx = 1;
+		gbc_jftfFecha.gridy = 2;
+		add(jftfFecha, gbc_jftfFecha);
+
+		JLabel lblNewLabel_3 = new JLabel("");
+		GridBagConstraints gbc_lblNewLabel_3 = new GridBagConstraints();
+		gbc_lblNewLabel_3.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_3.gridx = 0;
+		gbc_lblNewLabel_3.gridy = 3;
+		add(lblNewLabel_3, gbc_lblNewLabel_3);
+
+		JLabel lblNewLabel_4 = new JLabel("Limite:");
+		GridBagConstraints gbc_lblNewLabel_4 = new GridBagConstraints();
+		gbc_lblNewLabel_4.anchor = GridBagConstraints.EAST;
+		gbc_lblNewLabel_4.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_4.gridx = 0;
+		gbc_lblNewLabel_4.gridy = 4;
+		add(lblNewLabel_4, gbc_lblNewLabel_4);
+
+		jspinner = new JSpinner();
+		GridBagConstraints gbc_jspinner = new GridBagConstraints();
+		gbc_jspinner.gridwidth = 6;
+		gbc_jspinner.insets = new Insets(0, 0, 5, 0);
+		gbc_jspinner.gridx = 1;
+		gbc_jspinner.gridy = 4;
+		add(jspinner, gbc_jspinner);
+
+		JLabel lblNewLabel_5 = new JLabel("Saldo:");
+		GridBagConstraints gbc_lblNewLabel_5 = new GridBagConstraints();
+		gbc_lblNewLabel_5.anchor = GridBagConstraints.EAST;
+		gbc_lblNewLabel_5.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_5.gridx = 0;
+		gbc_lblNewLabel_5.gridy = 5;
+		add(lblNewLabel_5, gbc_lblNewLabel_5);
+
+		jslider = new JSlider();
+		GridBagConstraints gbc_jslider = new GridBagConstraints();
+		gbc_jslider.gridwidth = 6;
+		gbc_jslider.insets = new Insets(0, 0, 5, 0);
+		gbc_jslider.gridx = 1;
+		gbc_jslider.gridy = 5;
+		add(jslider, gbc_jslider);
+
+		JLabel lblNewLabel_6 = new JLabel("TipoContrato:");
+		GridBagConstraints gbc_lblNewLabel_6 = new GridBagConstraints();
+		gbc_lblNewLabel_6.anchor = GridBagConstraints.EAST;
+		gbc_lblNewLabel_6.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_6.gridx = 0;
+		gbc_lblNewLabel_6.gridy = 6;
+		add(lblNewLabel_6, gbc_lblNewLabel_6);
+
+		jtfTipoContrato = new JTextField();
+		jtfTipoContrato.setEnabled(false);
+		GridBagConstraints gbc_jtfTipoContrato = new GridBagConstraints();
+		gbc_jtfTipoContrato.gridwidth = 5;
+		gbc_jtfTipoContrato.insets = new Insets(0, 0, 5, 5);
+		gbc_jtfTipoContrato.fill = GridBagConstraints.HORIZONTAL;
+		gbc_jtfTipoContrato.gridx = 1;
+		gbc_jtfTipoContrato.gridy = 6;
+		add(jtfTipoContrato, gbc_jtfTipoContrato);
+		jtfTipoContrato.setColumns(10);
+
+		JButton btnTipoContrato = new JButton("Selecciona");
+		GridBagConstraints gbc_btnTipoContrato = new GridBagConstraints();
+		gbc_btnTipoContrato.insets = new Insets(0, 0, 5, 0);
+		gbc_btnTipoContrato.gridx = 6;
+		gbc_btnTipoContrato.gridy = 6;
+		add(btnTipoContrato, gbc_btnTipoContrato);
+
+		JLabel lblNewLabel_7 = new JLabel("Usuario:");
+		GridBagConstraints gbc_lblNewLabel_7 = new GridBagConstraints();
+		gbc_lblNewLabel_7.anchor = GridBagConstraints.EAST;
+		gbc_lblNewLabel_7.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_7.gridx = 0;
+		gbc_lblNewLabel_7.gridy = 7;
+		add(lblNewLabel_7, gbc_lblNewLabel_7);
+
+		JLabel lblNewLabel_8 = new JLabel("New label");
+		GridBagConstraints gbc_lblNewLabel_8 = new GridBagConstraints();
+		gbc_lblNewLabel_8.gridwidth = 5;
+		gbc_lblNewLabel_8.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_8.gridx = 1;
+		gbc_lblNewLabel_8.gridy = 7;
+		add(lblNewLabel_8, gbc_lblNewLabel_8);
+
+		JButton btnUsuario = new JButton("Selecciona");
+		GridBagConstraints gbc_btnUsuario = new GridBagConstraints();
+		gbc_btnUsuario.insets = new Insets(0, 0, 5, 0);
+		gbc_btnUsuario.gridx = 6;
+		gbc_btnUsuario.gridy = 7;
+		add(btnUsuario, gbc_btnUsuario);
+
+		JButton btnNewButton = new JButton("Guardar");
+		GridBagConstraints gbc_btnNewButton = new GridBagConstraints();
+		gbc_btnNewButton.insets = new Insets(0, 0, 0, 5);
+		gbc_btnNewButton.gridx = 3;
+		gbc_btnNewButton.gridy = 8;
+		add(btnNewButton, gbc_btnNewButton);
+
+		sincronizarSpinnerYSlider();
+	}
+
+	public void sincronizarSpinnerYSlider() {
+		jspinner.addChangeListener(new ChangeListener() {
+			public void stateChanged(ChangeEvent e) {
+				int valorMaximo = (int) jspinner.getValue();
+				jslider.setMaximum(valorMaximo);
+				
+			} 
+		});
+	}
+
+}
